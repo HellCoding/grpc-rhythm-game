9060a4b6cf3d3fc1e4ccd780652fcef470654428
is.jung <copu21@gmail.com>
2025-11-03 21:00:00 +0900

    Initialize gRPC project with Protobuf schema (GameService, LiveService, ScoreService)

---
+++ src/main/java/com/rhythmgame/RhythmGameApplication.java
@@ -0,0 +1,11 @@
+package com.rhythmgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhythmGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RhythmGameApplication.class, args);
+    }
+}
